6cfde67f6905273be05b4852c65221186d0bd5c0
jadashi97 <njadauniversal23@yahoo.com>
2023-03-02 21:59:18 -0600

    added root and more boiler code

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
+
+import Root from "./routes/root";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
